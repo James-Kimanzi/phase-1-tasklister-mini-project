82f63d6332ed1ffed34b9f6386c0d9954209bbe1
James <jameskimanzi@gmail.com>
2024-01-24 03:19:02 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+//
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form = document.querySelector ('form');
+    form.addEventListener('submit', (e) =>{
+  e.preventDefault();
+  buildTaskForm (e.target['new-task-description'].value);
+  form.reset()
+  });
+
+
+  const buildTaskForm = (task) => {
+  let p = document.createElement ('p');
+  let btn = document.createElement ('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent= 'X';
+  p.textContent =  `${task}  `;
+  p.appendChild(btn);
+  document.querySelector('#tasks').appendChild(p);
+  };
+
+  const handleDelete = (e) => {
+    e.target.parentNode.remove()
+  };
+  
 });
